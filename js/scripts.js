92ca70a2d0164c2d12a62f1089620bca1e0cbcd8
Hristo Dzhambov and Patrick Kille <hristo@>
2020-01-29 11:27:11 -0800

    added total, player switch, and roll fail functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,22 @@
 // Game Object
 function gameObj() {
   this.players = [],
-  this.id = id
+  this.playerId = 0;
+}
+
+gameObj.prototype.assignId = function() {
+  this.playerId += 1;
+  return this.playerId;
 }
 
 gameObj.prototype.addPlayer = function(player) {
   player.id = this.assignId();
   this.players.push(player);
-}
-
-gameObj.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId;
-}
-
-gameObj.prototype.switchPlayer = function() {
-    if (this.players[id] === 1) {
-      return 0;
-    } else {
-      return 1;
-    }
   }
 
 //Player/Computer Objects
 function playerObj() {
-  this.total = total
+  this.total = 0
 }
 
 //RNG
@@ -32,10 +24,44 @@ function diceRoll() {
   return Math.floor(Math.random()*6) + 1;
 }
 
-function pass(gameObj, score) {
-  gameObj.players.total += score;
-  var newID = gameObj.switchPlayer();
-  return newID;
+function pass(idNumber) {
+  if (idNumber === 1) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
 //UI
+$(document).ready(function(){
+  var Game = new gameObj();
+  var playerOne = new playerObj();
+  var playerComp = new playerObj();
+  var score = 0;
+  var playerNum = 0;
+  var currentPlayer;
+
+  Game.addPlayer(playerComp);
+  Game.addPlayer(playerOne);
+
+  $("#roll").click(function(){
+    var roll = diceRoll();
+    if (roll === 1) {
+      score = 0;
+      currentPlayer = Game.players[playerNum];
+      playerNum = pass(currentPlayer.id)
+      console.log("Player:"+currentPlayer.id);
+    } else {
+      score += roll;
+      console.log("Roll:"+roll);
+      console.log("Score:"+score);
+    }
+  });
+
+  $("#hold").click(function(){
+    currentPlayer = Game.players[playerNum];
+    currentPlayer.total += score;
+    score = 0;
+    playerNum = pass(currentPlayer.id)
+  });
+});
